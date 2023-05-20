d3d4874040989bb1af0f42ddf10dbb4ca5e106a7
Marco Aurelio S B <128655763+marcoa5432@users.noreply.github.com>
2023-05-19 22:16:19 -0300

    Add files via upload

--- Altenticação/script.js
+++ Altenticação/script.js
@@ -2,12 +2,30 @@ function validarFormulario (event) {
     event.preventDefault();
     
     const email = window.document.getElementById('email');
-    const password = window.document.getElementById('password').value;
-    const confirmPassword = window.document.getElementById("confirmPassword").value;
+    const password = window.document.getElementById('password');
+    const confirmPassword = window.document.getElementById("confirmPassword");
+    const messageEmail= window.document.getElementsByClassName("message-email");
+    const messagePassword = window.document.getElementsByClassName('message-password');
+    const messageConfirmPassword = window.document.getElementsByClassName('message-confirmPassword');
+    const messageCheckPassword = window.document.getElementsByClassName('message-fields-pswd');
 
     console.log('valores : ', email, password, confirmPassword);
-    if(!email.value == '') {
-         email.classlist.add('error');
+    if(email.value == '') {
+         email.classList.add('error');
+         messageEmail[0].classList.add('show-message');
     }
+if(password.value == '') {
+    password.classList.add('error');
+    messagePassword[0].classList.add('show-message');
+}   
+
+if(confirmPassword.value =='') {
+    confirmPassword.classList.add('error');
+    messageConfirmPassword[0].classList.add('show-message');
+}
+
+if(password.value == confirmPassword.value) {
+
+}
 
 }
